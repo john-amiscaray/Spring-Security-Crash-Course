4525a4b75d0926d419e177605c6fb267655db971
john-amiscaray <john_ernest_amiscaray@outlook.com>
2021-10-11 13:53:44 -0400

    fixed circular dependencies involving password encoding

---
+++ spring-security-demo/src/main/java/me/john/amiscaray/springsecuritydemo/config/PasswordEncoderConfig.java
@@ -0,0 +1,18 @@
+package me.john.amiscaray.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+
+        return new BCryptPasswordEncoder(10);
+
+    }
+
+}
